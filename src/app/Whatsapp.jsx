22e26344bb5ea2223e9a3b7f01d7a718cb9d91b6
rajurao <rajurao1107@gmail.com>
2025-07-04 11:09:03 +0530

    commit changes

--- src/app/Whatsapp.jsx
+++ src/app/Whatsapp.jsx
@@ -8,7 +8,7 @@ export default function Whatsapp() {
         rounded-full shadow-lg  transition"
       aria-label="Scroll to top"
     >
-      <a href="https://wa.me/919967267567">
+      <a href="https://wa.me/919970939063">
         <BsWhatsapp />
       </a>
     </button>
