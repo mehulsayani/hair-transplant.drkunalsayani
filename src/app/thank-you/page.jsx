039b3178b29a74f16584d3ea600f24afb6fd9da7
rajurao <rajurao1107@gmail.com>
2025-07-30 11:04:39 +0530

    commit changes

--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -4,6 +4,21 @@ import { CheckCircle } from 'lucide-react'; // Optional: use any icon lib
 export default function page() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white px-4 text-center">
+      <head>
+          {/* Google Ads Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16907003627"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16907003627');
+          `}
+        </Script>
+      </head>
       <CheckCircle className="text-green-500 w-16 h-16 mb-4" />
       <h1 className="text-3xl font-bold mb-2">Thank You for Booking!</h1>
       <p className="text-gray-600 mb-4 max-w-md">
